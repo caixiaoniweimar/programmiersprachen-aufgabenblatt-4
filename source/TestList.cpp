200eaae15087fba9b7bf1ef8e9da352f49554e70
caixiaoniweimar <710389740@qq.com>
2018-06-06 17:44:31 +0200

    add aufgabe 4.2 Test

--- source/TestList.cpp
+++ source/TestList.cpp
@@ -64,7 +64,58 @@ TEST_CASE("test_clear","[test_clear]"){
 	REQUIRE(list.empty()==true);
 }
 
+// Aufgabe 4.6
+TEST_CASE("should be an empty range after default construction","[iterators]"){ 
+	List<int> list;
+	auto b=list.begin();
+	auto e=list.end();
+	REQUIRE(b==e);
+}
+
+TEST_CASE("provide access to the first elemen","[iterators]"){
+	List<int> list;
+	list.push_front(42);
+	REQUIRE( 42 == *list.begin());
+}
+
+// Aufgabe 4.7
+TEST_CASE("beurteilen zwei List ob gleich","[List_ob_gleich]"){
+	List<int> list1;
+	List<int> list2;
+	REQUIRE( list1==list2);
+
+	list1.push_back(1);
+	list2.push_back(1);
+	REQUIRE( list1==list2 );
+	list2.push_back(2);
+	list1.push_back(3);
+	REQUIRE( list1!=list2 );
+
+	List<int> list4;
+	List<int> list3;
+	list3.push_back(1);
+	REQUIRE( list4!=list3);
+	list4.push_back(2);
+	list3.pop_back();
+	REQUIRE( list4!=list3);
+}
+
+// Aufgabe 4.8
+TEST_CASE("copy constructor","[constructor]"){
+	List<int> list;
+	list.push_front(1);
+	list.push_front(2);
+	list.push_front(3);
+	list.push_front(4);
+	List<int> list2{list};
+	REQUIRE( list==list2); 
+}
 
 int main(int argc, char* argv[]){
 	return Catch::Session().run(argc,argv);
 }
+
+
+
+
+
