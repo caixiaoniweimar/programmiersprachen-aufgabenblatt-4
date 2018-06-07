6086e17234eed854edc7a64e1f6b5f5298d12f7a
caixiaoniweimar <710389740@qq.com>
2018-06-07 23:52:59 +0200

    Add aufgabe 4.10

--- source/List.hpp
+++ source/List.hpp
@@ -2,8 +2,11 @@
 #define LIST_HPP
 #include <cstddef>
 #include <list>
+#include <vector>
+#include <initializer_list>
+#include <algorithm>
 using namespace std;
-//不要忘记 每个Klasse的成员变量的初始化(通过Initialisierungslist)
+//不要忘记 每个Klasse的成员变量的初始化(通过Initialisierungslist) 
 template <typename T>
 class List;
 
@@ -39,17 +42,27 @@ class ListIterator{
 			return (*node).value;  // this->pointer
 			//return (*this).value;					//指向相同的结果
 		}
+
+		/*int a = 5;
+
+		int& int_ref =  a; // direkte referenz
+		int* int_ptr = &a; // indirekte referenz
+
+		std::cout << int_ref;  // direkter zugriff
+		std::cout << *int_ptr; // indirekter zugriff
+		*/
+
 		pointer operator->() const{  //取所指节点中数据的地址  c2->radius();
-			return &( operator*() ); 		
-			//return &( (*this).value );
+			return &( (*this).value ); 		
 		}
 
+		// pre-increment
 		Self& operator++() {		//前置++操作符,指向下一个节点   ++iterator 返回 增加后的引用！！
 			//node=node->next;
 			node =(*node).next;     //++i iterator推迟
 			return *this;
 		}
-		
+		//post-increment
 		Self operator++(int) {		//后置++操作符，指向下一个节点	 iterator++    返回之前的值
 			 Self neue = *this;    //i++ 
 			 ++ *this;				
@@ -119,7 +132,6 @@ class List{
 			size_{ 0 },
 			first_{ nullptr },
 			last_{ nullptr } { 
-
 				auto i=list.begin();
 				while( i!=list.end() ){
 					push_back(*i);
@@ -207,11 +219,82 @@ class List{
         ~List(){ clear(); }; //Destruktor mit clear()
 
 // Aufgabe 4.9
-       	/*void insert( ListNode<T>* obj, iterator position){
-       		ListNode<T>* obj=new ListNode<T>{obj, (*position).prev, (*position)};
+       	void insert( T const& objekt, ListIterator<T> const& position){ //知道iterator 就可以getnode!!得到这个位置的node
+       		
+       		ListNode<T>* node = position.get_node();
+       		if( (*node).prev == nullptr ){
+       			/*ListNode<T>* neue_objekt=new ListNode<T>{objekt,nullptr,node}; //说明node是第一个元素
+       			(*node).prev=neue_objekt;
+       			first_=neue_objekt;*/
+       			push_front(objekt);
+       		}
+       		if( node==nullptr ){
+       			push_back(objekt);
+       		}
+       		else{
+       			ListNode<T>* neue_objekt=new ListNode<T>{objekt, (*node).prev, node};
+       			(*(*node).prev).next=neue_objekt;
+       			(*node).prev=neue_objekt;
+       		}
+ 
+       		++size_;
+        } 
+// Aufgabe 4.10
+        void reverse(){
+        	ListNode<T>* node = first_ ;  // node为first_第一个元素
+        	ListNode<T>* temp = nullptr;  // 暂时的对象 为 temp 为空
 
-        } */
+        	while( node != nullptr){		// 只要node不为空 当node到end() 则为空
+        		temp = (*node).next;		// temp是 node->next
+        		(*node).next = (*node).prev; // node->next 是 node->prev 原来的node已经到了(原来的node.next)的后面
+        		(*node).prev = temp;		 // node的前面 是temp
+        		if(temp==nullptr){     //当temp为空,则node->next为空 则此时的temp为back() 最后一个元素
+        			last_=first_;		// 最后的元素等于第一个元素
+        			first_=node;		// 第一个元素等于node 
+        		}
+        		node=temp;			//每次改变循环条件, node等于temp 等于node.next 一直往后面前进
+        	}
+    	}
 
+// Aufgabe 4.12							//?
+    	/*void Swap( List<T>& lis){
+    		swap(first_ , lis.first_);
+    		swap(last_ , lis.last_);
+    	}*/
+    	List<T>& operator= ( List<T>& list1) {	
+    		clear();
+    		for(auto i=list1.begin(); i!=list1.end(); ++i){
+    			push_back(*i);
+    		}
+    		return *this;
+    	}
+    	/*List<T>& operator= ( List<T> lis){
+    		//clear();
+    		Swap(lis);
+    		return *this;
+    	}*/
+
+// Aufgabe 4.13
+  		List(List<T>&& list):  // move-Konstruktor
+  			size_(list.size_),
+  			first_(list.first_),
+  			last_(list.last_)
+  			{
+  				list.size_=0;
+  				list.first_=nullptr;
+  				list.last_=nullptr;
+  				cout<<"1"<<endl;
+  			}
+
+// Aufgabe 4.14
+		List(initializer_list<T> list):
+			size_(0),
+			first_(nullptr),
+			last_(nullptr) {
+				for( auto i=list.begin(); i!=list.end(); ++i){
+					push_back(*i);
+				}
+			}
 	private:
 	    std::size_t size_;
 		ListNode<T>* first_;  //Pointer,Zeiger first_ ist ein Zeiger auf ListNode   // *first_ : Node(Element )
@@ -219,7 +302,7 @@ class List{
 };
 
 // Aufgabe 4.7
-template<typename T>
+template <typename T>
 bool operator==(List<T> const& xs,List<T> const& ys){
 		if(xs.size()==ys.size()){
 				auto i=xs.begin();
@@ -240,24 +323,36 @@ bool operator==(List<T> const& xs,List<T> const& ys){
 		return true;
 }
 
-template<typename T>
+template <typename T>
 bool operator !=(List<T> const& xs, List<T> const& ys){
 	if(xs==ys){
 		return false;
 	}
 	else 
 		return true;
-}                 
+}               
 
+template<typename T>
+List<T> reverse(List<T> const& list) {
+	List<T> list1(list);
+	list1.reverse();
+	return list1;	
+}
 
-
-
-
-
-
-
-
-
-
-
+template<typename T>
+List<T> operator+(List<T> const& lhs, List<T> const& rhs){  //有两个Uebergabeparamter-> freie Funktion!!
+			//List<T> r(lhs);
+			List<T> r;
+			for( auto i=lhs.begin(); i!=lhs.end(); ++i){
+					r.push_back(move(*i));
+			}
+			for( auto i=rhs.begin(); i!=rhs.end(); ++i){
+					r.push_back(move(*i));
+			}
+			return r;
+}
+//可以移动而非拷贝对象的能力。在其中某些情况下，对象拷贝后就立即被销毁了，
+//在这些情况下，移动而非拷贝对象会大幅度提升性能
+//C++ 标准库使用比如vector::push_back 等这类函数时,会对参数的对象进行复制,
+//连数据也会复制.这就会造成对象内存的额外创建
 #endif
